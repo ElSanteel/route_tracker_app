9480749b3051f8b8fedbd37da4624e898363b80d
ElSanteel <alsantel.ma@gmail.com>
2024-05-18 12:37:05 +0300

    refactor location service

--- lib/utils/location_service.dart
+++ lib/utils/location_service.dart
@@ -4,45 +4,53 @@ class LocationService {
   Location location = Location();
 
   // step 1 : method to check if location service is enabled or not
-  Future<bool> checkAndRequestLocationService() async {
+  Future<void> checkAndRequestLocationService() async {
     // check if location service is enabled or not
     var isServiceEnabled = await location.serviceEnabled();
     if (!isServiceEnabled) {
       // request location service
       isServiceEnabled = await location.requestService();
       if (!isServiceEnabled) {
-        return false;
+        throw LocationServiceException();
       }
     }
-    return true;
   }
 
   // step 2 : method to check if location permission is granted or not
-  Future<bool> checkAndRequestLocationPermission() async {
+  Future<void> checkAndRequestLocationPermission() async {
     // check if location permission is granted or not
     var permissionStatus = await location.hasPermission();
     // check if the permission is denied forever
     if (permissionStatus == PermissionStatus.deniedForever) {
-      return false;
+      LocationServiceException();
     }
     // check if the permission is denied
     if (permissionStatus == PermissionStatus.denied) {
       // request location permission
       permissionStatus = await location.requestPermission();
-      // checking the permission status ( way one )
-      return permissionStatus == PermissionStatus.granted;
+      // check if the permission is not granted
+      if (permissionStatus != PermissionStatus.granted) {
+        // throw an exception
+        throw LocationPermissionException();
+      }
     }
-    // return true if the permission is granted and the user allowed the permission
-    return true;
   }
 
   // notice here in this commit we don't need this method now cuz we don't want a stream of data
-  void getRealTimeLocationData(void Function(LocationData)? onData) {
+  void getRealTimeLocationData(void Function(LocationData)? onData) async {
+    await checkAndRequestLocationService();
+    await checkAndRequestLocationPermission();
     location.onLocationChanged.listen(onData);
   }
 
   // step 3 : method to get the location data
   Future<LocationData> getLocation() async {
+    await checkAndRequestLocationService();
+    await checkAndRequestLocationPermission();
     return await location.getLocation();
   }
 }
+
+class LocationServiceException implements Exception {}
+
+class LocationPermissionException implements Exception {}
