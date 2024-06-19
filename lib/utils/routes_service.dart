b02f3fa1262579547bd1f3bbf7d3692f7b73eaba
ElSanteel <alsantel.ma@gmail.com>
2024-06-19 18:48:55 +0300

    fetch route part 3 and 4

--- lib/utils/routes_service.dart
+++ lib/utils/routes_service.dart
@@ -1,46 +1,44 @@
+import 'dart:convert';
+
+import 'package:route_tracker_app/models/location_info/location_info.dart';
 import 'package:route_tracker_app/models/routes_model/routes_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:route_tracker_app/models/routes_modifier.dart';
 
 class RoutesService {
   final String baseUrl =
       'https://routes.googleapis.com/directions/v2:computeRoutes';
   final String apiKey = 'AIzaSyBmhDvQXo3iFJt-j0v9VrgEihwFU6_Qa1E';
 
-  Future<RoutesModel> fetchRoutes({}) {
-    Uri url =Uri.parse(baseUrl);
-    Map<String,String> header={
-      'Content-Type':'application/json',
+  Future<RoutesModel> fetchRoutes(
+      {required LocationInfo origin,
+      required LocationInfo destination,
+      RoutesModifier? routeModifiers}) async {
+    Uri url = Uri.parse(baseUrl);
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
       'X-Goog-Api-Key': apiKey,
-      'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
+      'X-Goog-FieldMask':
+          'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
     };
-    Map<String,dynamic>body= {
-      "origin":{
-    "location":{
-      "latLng":{
-        "latitude": 37.419734,
-        "longitude": -122.0827784
-      }
+    Map<String, dynamic> body = {
+      "origin": origin.toJson(),
+      "destination": destination.toJson(),
+      "travelMode": "DRIVE",
+      "routingPreference": "TRAFFIC_AWARE",
+      "computeAlternativeRoutes": false,
+      "routeModifiers": routeModifiers != null
+          ? routeModifiers.toJson()
+          : RoutesModifier().toJson(),
+      "languageCode": "en-US",
+      "units": "IMPERIAL"
+    };
+
+    var response = await http.post(url, headers: headers, body: body);
+    if(response.statusCode == 200) {
+      return RoutesModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('No routes found');
     }
-  },
-  "destination":{
-    "location":{
-      "latLng":{
-        "latitude": 37.417670,
-        "longitude": -122.079595
-      }
-    }
-  },
-  "travelMode": "DRIVE",
-  "routingPreference": "TRAFFIC_AWARE",
-  "departureTime": "2023-10-15T15:01:23.045123456Z",
-  "computeAlternativeRoutes": false,
-  "routeModifiers": {
-    "avoidTolls": false,
-    "avoidHighways": false,
-    "avoidFerries": false
-  },
-  "languageCode": "en-US",
-  "units": "IMPERIAL"
-};
   }
 }
