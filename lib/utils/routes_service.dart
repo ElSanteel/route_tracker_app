aca641db87a004ac56d8c9ceb4d339c59404bb7e
ElSanteel <alsantel.ma@gmail.com>
2024-06-19 18:22:02 +0300

    fetch routes part 2

--- lib/utils/routes_service.dart
+++ lib/utils/routes_service.dart
@@ -8,6 +8,39 @@ class RoutesService {
 
   Future<RoutesModel> fetchRoutes({}) {
     Uri url =Uri.parse(baseUrl);
-    
+    Map<String,String> header={
+      'Content-Type':'application/json',
+      'X-Goog-Api-Key': apiKey,
+      'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
+    };
+    Map<String,dynamic>body= {
+      "origin":{
+    "location":{
+      "latLng":{
+        "latitude": 37.419734,
+        "longitude": -122.0827784
+      }
+    }
+  },
+  "destination":{
+    "location":{
+      "latLng":{
+        "latitude": 37.417670,
+        "longitude": -122.079595
+      }
+    }
+  },
+  "travelMode": "DRIVE",
+  "routingPreference": "TRAFFIC_AWARE",
+  "departureTime": "2023-10-15T15:01:23.045123456Z",
+  "computeAlternativeRoutes": false,
+  "routeModifiers": {
+    "avoidTolls": false,
+    "avoidHighways": false,
+    "avoidFerries": false
+  },
+  "languageCode": "en-US",
+  "units": "IMPERIAL"
+};
   }
 }
